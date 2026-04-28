0329642590613c481ca3996129a773770e9b5fde
SomethingTan <somethingyt010@gmail.com>
2026-04-28 11:14:54 +0200

    add 0180

---
+++ Solutions/0180_multiply_numbers_exercise.py
@@ -0,0 +1,36 @@
+"""
+Opgave "multiply_numbers":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+-------
+
+Forandre koden i funktionen multiply_numbers() sådan at den gør hvad der står i dens dokumentation.
+
+-------
+
+Hvis du går i stå, så spørg google, de andre elever, en AI eller læreren.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Fortsæt derefter med den næste fil.
+"""
+
+
+def multiply_numbers(start, multiplier, upper_limit):
+    """
+    Beginning with the start value, this function prints the current value,
+    then multiplies it with multiplier, prints the new current value etc.
+    The function stops when the current value is greater than the upper limit"""
+    next_val = start
+    while next_val < upper_limit:
+        print(next_val)
+        next_val = next_val*multiplier
+
+
+
+
+
+
+multiply_numbers(7, 4, 63553)
