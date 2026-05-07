fd0b3e12cf3f4f9a20e824b66873baa46b765352
SomethingTan <somethingyt010@gmail.com>
2026-05-07 13:37:21 +0200

    add 450,460,500,510,520,530,540,570

---
+++ Solutions/0460_cars_help.py
@@ -0,0 +1,36 @@
+"""
+Opgave "Cars" (med lidt hjælp):
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Replace the zeros in the program code with your own code.
+
+Definer en funktion drive_car(), der udskriver en bils motorlyd (f.eks. "roooaar")
+
+I hovedprogrammet:
+    Definer variabler, som repræsenterer antallet af hjul og den maksimale hastighed for 2 forskellige biler
+    Udskriv disse egenskaber for begge biler
+    Kald derefter funktionen motorlyd
+
+Hvis du går i stå, kan du spørge google, de andre elever, en AI eller læreren.
+Hvis du stadig er gået i stå, skal du åbne 0470_cars_solution.py og sammenligne den med din løsning.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Team-besked til din lærer: <filename> done
+Fortsæt derefter med den næste fil."""
+
+def drive_car():
+    print("vrooom")  # print a sound
+
+
+car1Wheels = 4
+car1Speed = 230
+car2Wheels = 3
+car2Speed = 228
+
+print(f"Car 1\nWheels : {car1Wheels}\nTop Speed : {car1Speed}\n")
+print(f"Car 2\nWheels : {car2Wheels}\nTop Speed : {car2Speed}\n")
+print()
+drive_car()
