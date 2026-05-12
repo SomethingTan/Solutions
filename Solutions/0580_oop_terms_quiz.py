482dfdd21b8a4a09192848d1d16997f913c99a7a
SomethingTan <somethingyt010@gmail.com>
2026-05-12 11:08:35 +0200

    add 580

---
+++ Solutions/0580_oop_terms_quiz.py
@@ -0,0 +1,44 @@
+"""
+Kør dette program.
+Tilføj oop-relaterede kommentarer til denne kode.
+    Eksempler:
+        class definition / klasse definition
+        constructor / konstruktor
+        inheritance / nedarvning
+        object definition / objekt definition
+        attribute / attribut
+        method / metode
+        polymorphism / polymorfisme
+        encapsulation: protected attribute / indkapsling: beskyttet attribut
+        encapsulation: protected method / indkapsling: beskyttet metode
+"""
+
+
+class Building: #class definition
+    def __init__(self, area, floors, value): #constructor
+        self.area = area # Attribute
+        self.floors = floors # Attribute
+        self._value = value # Protected Attribute
+
+    def renovate(self):
+        print("Installing an extra bathroom...")
+        self._adjust_value(10) # Protected Method
+
+    def _adjust_value(self, percentage): # Protected Method
+        self._value *= 1 + (percentage / 100) # Protected Attribute
+        print(f'Value has been adjusted by {percentage}% to {self._value:.2f}\n')
+
+
+class Skyscraper(Building): #inheritance from "Building"
+
+    def renovate(self): # Polymorphism
+        print("Installing a faster elevator.")
+        self._adjust_value(6) # Protected Method
+
+
+small_house = Building(160, 2, 200000) # Object definition
+skyscraper = Skyscraper(5000, 25, 10000000) # Object definition
+
+for building in [small_house, skyscraper]:
+    print(f'This building has {building.floors} floors and an area of {building.area} square meters.')
+    building.renovate()
